240a8115bb1baab3a0adc700ead950122f660e32
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-04 18:12:54 -0400

    Rendered film cards

--- src/components/film-list-extra.js
+++ src/components/film-list-extra.js
@@ -1,21 +1,13 @@
-import {createFilmCard} from "./film-card.js";
+import {renderFilmCards} from "./film-card.js";
 
-const FILM_EXTRA_COUNT = 2;
-
-const getFilmCards = () => {
-  let markup = ``;
-  for (let i = 0; i < FILM_EXTRA_COUNT; i++) {
-    markup += createFilmCard();
-  }
-  return markup;
-};
+const QUANTITY_FILMS_IN_CARD = 2;
 
 export const createFilmExtra = () => {
   return (
     `<section class="films-list--extra">
       <h2 class="films-list__title">Top rated</h2>
       <div class="films-list__container">
-        ${getFilmCards()}
+        ${renderFilmCards(QUANTITY_FILMS_IN_CARD)}
       </div>
     </section>`
   );
