38a04a4ecfbbcb80729e20db15e0480ad2c31a3f
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-22 01:26:21 -0400

    Created class "FilmList"

--- src/components/films/film-list.js
+++ src/components/films/film-list.js
@@ -1,17 +1,35 @@
-import {createButtonShowMore} from "./button-showmore.js";
-import {createExtraSections} from "./film-list-extra.js";
+import {createElement} from "../../util/dom-util";
 
-export const createFilmList = (films, filmCards, onShowMoreButtonClick) => {
-  return (
-    `<section class="films">
-      <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-        <div class="films-list__container">
-          ${filmCards}
-        </div>
-        ${createButtonShowMore(onShowMoreButtonClick)}
-      </section>
-      ${createExtraSections(films)}
-    </section>`
-  );
-};
+export default class FilmList {
+  constructor() {
+    // this._films = films;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return (
+      `<section class="films">
+        <section class="films-list">
+          <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+          <div class="films-list__container">
+
+          </div>
+
+        </section>
+
+      </section>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
