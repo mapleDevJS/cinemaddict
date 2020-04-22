a007b13ecb5e18e04b338ac66da4d8f7ba1c16c5
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-22 01:25:36 -0400

    Created class "Sort"

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,9 +1,39 @@
-export const createSiteSort = () => {
-  return (
-    `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
-    </ul>`
-  );
-};
+import {createElement} from "../util/dom-util";
+
+const SORT_TYPES = [
+  `by default`,
+  `by date`,
+  `by rating`
+];
+
+export default class Sort {
+  constructor() {
+    this._element = null;
+  }
+
+  _getSortItems() {
+    return SORT_TYPES.map((type) =>
+      `<li><a href="#" class="sort__button sort__button--active">Sort ${type}</a></li>`
+    ).join(`\n`);
+  }
+
+  getTemplate() {
+    return (
+      `<ul class="sort">
+        ${this._getSortItems()}
+      </ul>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
