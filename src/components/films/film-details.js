8fd3342e1947fdc5f6685916484607a1a512cc82
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-22 01:27:35 -0400

    Created class "FilmDetails"

--- src/components/films/film-details.js
+++ src/components/films/film-details.js
@@ -1,111 +1,156 @@
+import {createElement} from "../../util/dom-util";
+import {getPlurals, getFullDate} from "../../util/util";
 
-const getGenreTitle = (genres) => {
-  return (genres.length > 1) ? `Genres` : `Genre`;
-};
+const CARD_CONTROLS = new Map([
+  [`watchlist`, `Add to watchlist`],
+  [`watched`, `Already watched`],
+  [`favorite`, `Add to favorites`]
+]);
 
-const getGenreMarkup = (genres) => {
-  let markup = ``;
-  for (const genre of genres) {
-    markup += `<span class="film-details__genre">${genre}</span>`;
+const EMOJIS = [
+  `smile`,
+  `sleeping`,
+  `puke`,
+  `angry`
+];
+
+export default class FilmDetails {
+  constructor(film) {
+    this._film = film;
+    this._element = null;
   }
-  return markup;
-};
 
-export const createFilmDetailsTemplate = ({
-  poster,
-  age,
-  title,
-  original,
-  rating,
-  writers,
-  actors,
-  release,
-  runtime,
-  country,
-  genres,
-  description,
-  comments
-}) => {
-  return `
-    <section class="film-details">
-      <form class="film-details__inner" action="" method="get">
+  _getGenresMarkup() {
+    return this._film.genres.reduce((prev, item) => {
+      return (
+        `${prev}<span class="film-details__genre">${item}</span>`
+      );
+    }, ``);
+  }
+
+  _getFilmDetails() {
+    const dataList = [
+      {
+        name: `Director`,
+        value: this._film.director
+      },
+      {
+        name: getPlurals(this._film.writers.length, [`Writer`, `Writers`]),
+        value: this._film.writers
+      },
+      {
+        name: getPlurals(this._film.actors.length, [`Actor`, `Actors`]),
+        value: this._film.actors
+      },
+      {
+        name: `Release`,
+        value: getFullDate(this._film.release)
+      },
+      {
+        name: `Runtime`,
+        value: this._film.runtime
+      },
+      {
+        name: `Country`,
+        value: this._film.country
+      },
+      {
+        name: getPlurals(this._film.genres.length, [`Genre`, `Genres`]),
+        value: this._getGenresMarkup()
+      },
+    ];
+
+    const rowsMarkup = dataList
+      .reduce((prev, {name, value}) => {
+        return (
+          `${prev}<tr class="film-details__row">
+            <td class="film-details__term">${name}</td>
+            <td class="film-details__cell">${value}</td>
+          </tr>`
+        );
+      }, ``);
+
+    return (
+      `<table class="film-details__table">
+        ${rowsMarkup}
+      </table>`
+    );
+  }
+
+  _getComments() {
+    return (
+      `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile" checked>
+      <label class="film-details__emoji-label" for="emoji-smile">
+        <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
+      </label>`
+    );
+  }
+
+  _getEmojiControls() {
+    return EMOJIS.map((emoji) =>
+      `<input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
+        <label class="film-details__emoji-label" for="emoji-${emoji}">
+        <img src="./images/emoji/${emoji}.png" width="30" height="30" alt="emoji ${emoji}">
+      </label>`
+    ).join(`\n`);
+  }
+
+  _getCardControls() {
+    let markup = ``;
+    CARD_CONTROLS.forEach((value, key) => {
+      markup +=
+      `<input type="checkbox" class="film-details__control-input visually-hidden" id="${key}" name="${key}">
+      <label for="${key}" class="film-details__control-label film-details__control-label--${key}">${value}</label>`;
+    });
+
+    return markup;
+  }
+
+  getTemplate() {
+    return (
+      `<section class="film-details">
+        <form class="film-details__inner" action="" method="get">
         <div class="form-details__top-container">
           <div class="film-details__close">
             <button class="film-details__close-btn" type="button">close</button>
           </div>
           <div class="film-details__info-wrap">
             <div class="film-details__poster">
-              <img class="film-details__poster-img" src="./images/posters/${poster}" alt="">
+              <img class="film-details__poster-img" src="./images/posters/${this._film.poster}" alt="">
 
-              <p class="film-details__age">${age}</p>
+              <p class="film-details__age">${this._film.age}</p>
             </div>
 
             <div class="film-details__info">
               <div class="film-details__info-head">
                 <div class="film-details__title-wrap">
-                  <h3 class="film-details__title">${title}</h3>
-                  <p class="film-details__title-original">${original}</p>
+                  <h3 class="film-details__title">${this._film.title}</h3>
+                  <p class="film-details__title-original">${this._film.original}</p>
                 </div>
 
                 <div class="film-details__rating">
-                  <p class="film-details__total-rating">${rating}</p>
+                  <p class="film-details__total-rating">${this._film.rating}</p>
                 </div>
               </div>
 
               <table class="film-details__table">
-                <tr class="film-details__row">
-                  <td class="film-details__term">Director</td>
-                  <td class="film-details__cell">Anthony Mann</td>
-                </tr>
-                <tr class="film-details__row">
-                  <td class="film-details__term">Writers</td>
-                  <td class="film-details__cell">${writers}</td>
-                </tr>
-                <tr class="film-details__row">
-                  <td class="film-details__term">Actors</td>
-                  <td class="film-details__cell">${actors}</td>
-                </tr>
-                <tr class="film-details__row">
-                  <td class="film-details__term">Release Date</td>
-                  <td class="film-details__cell">${release}</td>
-                </tr>
-                <tr class="film-details__row">
-                  <td class="film-details__term">Runtime</td>
-                  <td class="film-details__cell">${runtime}</td>
-                </tr>
-                <tr class="film-details__row">
-                  <td class="film-details__term">Country</td>
-                  <td class="film-details__cell">${country}</td>
-                </tr>
-                <tr class="film-details__row">
-                  <td class="film-details__term">${getGenreTitle(genres)}</td>
-                  <td class="film-details__cell">
-                    ${getGenreMarkup(genres)}
-                  </td>
-                </tr>
+                ${this._getFilmDetails()}
               </table>
 
               <p class="film-details__film-description">
-                ${description}
+                ${this._film.description}
               </p>
             </div>
           </div>
 
           <section class="film-details__controls">
-            <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
-            <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
-
-            <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
-            <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
-
-            <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
-            <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
+            ${this._getCardControls()}
           </section>
         </div>
 
         <div class="form-details__bottom-container">
           <section class="film-details__comments-wrap">
-            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
+            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${this._film.comments.length}</span></h3>
 
             <ul class="film-details__comments-list"></ul>
 
@@ -119,30 +164,25 @@ export const createFilmDetailsTemplate = ({
               </label>
 
               <div class="film-details__emoji-list">
-                <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile" checked>
-                <label class="film-details__emoji-label" for="emoji-smile">
-                  <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
-                </label>
-
-                <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
-                <label class="film-details__emoji-label" for="emoji-sleeping">
-                  <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
-                </label>
-
-                <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
-                <label class="film-details__emoji-label" for="emoji-puke">
-                  <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
-                </label>
-
-                <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
-                <label class="film-details__emoji-label" for="emoji-angry">
-                  <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
-                </label>
+                ${this._getEmojiControls()}
               </div>
             </div>
           </section>
         </div>
       </form>
-    </section>
-  `;
-};
+    </section>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
