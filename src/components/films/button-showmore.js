462a194df941c570fda51d32db33a6035aa33d95
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-22 01:27:55 -0400

    Created class "ButtonShowMore"

--- src/components/films/button-showmore.js
+++ src/components/films/button-showmore.js
@@ -1,8 +1,24 @@
-export const createButtonShowMore = () => {
-  return (
-    `<button class="films-list__show-more">Show more</button>`
-  );
-};
+import {createElement} from "../../util/dom-util";
+export default class ButtonShowMore {
+  constructor() {
+    this._element = null;
+  }
 
-// export const addListenerOnShowMoreButton = (cb) =>
-//   document.querySelector(`.films-list__show-more`).addEventListener(`click`, cb);
+  getTemplate() {
+    return (
+      `<button class="films-list__show-more">Show more</button>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
