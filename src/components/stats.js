a623abfad6ade0c6e457c06b3f434f8520cfb319
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-22 01:25:57 -0400

    Created class "Stats"

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,6 +1,26 @@
-export const createFilmStats = (films) => {
-  const total = films.length;
-  return (
-    `<p>${total} movies inside</p>`
-  );
-};
+import {createElement} from "../util/dom-util";
+
+export default class Stats {
+  constructor(films) {
+    this._films = films;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return (
+      `<p>${this._films.length} movies inside</p>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
