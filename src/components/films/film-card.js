2186f5b814cf08e6b7e7b852a736d5f23c2c2b8a
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-24 22:32:09 -0400

    Refactoring film-card

--- src/components/films/film-card.js
+++ src/components/films/film-card.js
@@ -1,12 +1,11 @@
 import {createElement} from "../../util/dom-util";
-import {getPlurals} from "../../util/util";
+import {pluralize} from "../../util/util";
 
 const CARD_CONTROLS = new Map([
   [`add-to-watchlist`, `Add to watchlist`],
   [`mark-as-watched`, `Mark as watched`],
   [`favorite`, `Mark as favorite`]
-]
-);
+]);
 
 export default class FilmCard {
   constructor(film) {
@@ -14,12 +13,22 @@ export default class FilmCard {
     this._element = null;
   }
 
-  _getButtons() {
-    let markup = ``;
-    CARD_CONTROLS.forEach((value, key) => {
-      markup += `<button class="film-card__controls-item button film-card__controls-item--${key}">${value}</button>`;
-    });
-    return markup;
+  // _getButtons() {
+  //   let markup = ``;
+  //   CARD_CONTROLS.forEach((value, key) => {
+  //     markup += `<button class="film-card__controls-item button film-card__controls-item--${key}">${value}</button>`;
+  //   });
+  //   return markup;
+  // }
+
+  _getButton(key, value) {
+    return `<button class="film-card__controls-item button film-card__controls-item--${key}">${value}</button>`;
+  }
+
+  _getButtonsMarkup() {
+    return [...CARD_CONTROLS.entries()]
+      .map(([value, key]) => this._getButton(value, key))
+      .join(`\n`);
   }
 
   getTemplate() {
@@ -28,15 +37,15 @@ export default class FilmCard {
         <h3 class="film-card__title">${this._film.title}</h3>
         <p class="film-card__rating">${this._film.rating}</p>
         <p class="film-card__info">
-          <span class="film-card__year">${this._film.release.getFullYear()}</span>
+          <span class="film-card__year">${this._film.release}</span>
           <span class="film-card__duration">${this._film.runtime}</span>
           <span class="film-card__genre">${this._film.genres.shift()}</span>
         </p>
         <img src="./images/posters/${this._film.poster}" alt="" class="film-card__poster">
         <p class="film-card__description">${this._film.description}</p>
-        <a class="film-card__comments">${this._film.comments.length} ${getPlurals(this._film.comments.length, [`Comment`, `Comments`])}</a>
+        <a class="film-card__comments">${this._film.comments.length} ${pluralize(this._film.comments.length, [`Comment`, `Comments`])}</a>
         <form class="film-card__controls">
-          ${this._getButtons()}
+          ${this._getButtonsMarkup()}
         </form>
       </article>`
     );
