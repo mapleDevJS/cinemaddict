65d0684a745ee8ea3a74deea2026f65e5677a4a2
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-04-27 18:03:30 -0400

    Created FilmsListContainer class

---
+++ src/components/films/films-list-container.js
@@ -0,0 +1,10 @@
+import Abstract from "../abstract";
+
+export default class FilmsListContainer extends Abstract {
+
+  getTemplate() {
+    return (
+      `<div class="films-list__container"></div>`
+    );
+  }
+}
