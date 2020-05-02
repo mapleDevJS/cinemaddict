9fabae578d88dbbeb1744f42ea7e3c8c0608ca89
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-05-02 09:50:46 -0400

    Add SortTypeChamgeHandler

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,16 +1,28 @@
 import Abstract from "./abstract";
 
-const SORT_TYPES = [
-  `by default`,
-  `by date`,
-  `by rating`
-];
+export const SORT_TYPES = {
+  DEFAULT: `by default`,
+  DATE: `by date`,
+  RATING: `by rating`
+};
 
 export default class Sort extends Abstract {
+  constructor() {
+    super();
+
+    this._currenSortType = SORT_TYPES.DEFAULT;
+  }
+
   _getSortItems() {
-    return SORT_TYPES.map((type) =>
-      `<li><a href="#" class="sort__button sort__button--active">Sort ${type}</a></li>`
-    ).join(`\n`);
+    // return SORT_TYPES.map((type) =>
+    //   `<li><a href="#" class="sort__button sort__button--active">Sort ${type}</a></li>`
+    // ).join(`\n`);
+
+    let markup = ``;
+    for (let [key, value] of Object.entries(SORT_TYPES)) {
+      markup += `<li><a href="#" data-sort-type="${key}" class="sort__button sort__button--active">Sort ${value}</a></li>`;
+    }
+    return markup;
   }
 
   getTemplate() {
@@ -19,5 +31,27 @@ export default class Sort extends Abstract {
         ${this._getSortItems()}
       </ul>`
     );
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      // const sortType = evt.target.dataset.sortType;
+      const sortType = evt.target.dataset.sortType;
+
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      this._currenSortType = sortType;
+
+      handler(this._currenSortType);
+    });
   }
 }
