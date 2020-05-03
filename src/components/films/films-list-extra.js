1af67a50e5522c9292658410af7e6b2f152ee070
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-05-03 11:14:38 -0400

    Refactored FilmsListExtra

--- src/components/films/films-list-extra.js
+++ src/components/films/films-list-extra.js
@@ -1,4 +1,5 @@
 import Abstract from "../abstract";
+import {createElement} from "../../util/dom-util";
 
 export default class FilmsListExtra extends Abstract {
   constructor(title) {
@@ -10,8 +11,7 @@ export default class FilmsListExtra extends Abstract {
     return (
       `<section class="films-list--extra">
         <h2 class="films-list__title">${this._title}</h2>
-        <div class="films-list__container">
-        </div>
+        <div class="films-list__container"></div>
       </section>`
     );
   }
