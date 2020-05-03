396e899425c3a6e47f1283e82d9deee891042c0e
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-05-02 23:09:55 -0400

    Refactored component FilmsLustExtra

--- src/components/films/films-list-extra.js
+++ src/components/films/films-list-extra.js
@@ -10,6 +10,8 @@ export default class FilmsListExtra extends Abstract {
     return (
       `<section class="films-list--extra">
         <h2 class="films-list__title">${this._title}</h2>
+        <div class="films-list__container">
+        </div>
       </section>`
     );
   }
