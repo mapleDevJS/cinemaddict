7e8a6250d81265207122ab81e8e8cb931d4cafdd
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-05-08 12:18:55 -0400

    Fixed sorting

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,4 +1,5 @@
 import Abstract from "./abstract";
+import AbstractSmart from "./abstract-smart";
 
 export const SortType = {
   DEFAULT: `default`,
@@ -6,11 +7,12 @@ export const SortType = {
   RATING: `rating`
 };
 
-export default class Sort extends Abstract {
+export default class Sort extends AbstractSmart {
   constructor() {
     super();
 
     this._currentSortType = SortType.DEFAULT;
+    this._sortTypeChangeListener = null;
   }
 
   getTemplate() {
@@ -21,11 +23,16 @@ export default class Sort extends Abstract {
     );
   }
 
+  recoverListeners() {
+    this.setSortTypeChangeListener(this._sortTypeChangeListener);
+  }
+
   getSortType() {
     return this._currentSortType;
   }
 
   setSortTypeChangeListener(listener) {
+    this._sortTypeChangeListener = listener;
     this.getElement().addEventListener(`click`, (evt) => {
       evt.preventDefault();
 
@@ -42,6 +49,7 @@ export default class Sort extends Abstract {
       this._currentSortType = sortType;
 
       listener(this._currentSortType);
+      super.rerender();
     });
   }
 
