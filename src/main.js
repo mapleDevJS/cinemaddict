554d9819b8f9d68b258ce47d6f45e57b270d0e79
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2020-05-21 11:45:25 -0400

    Add listener to "Stats" menu item

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import UserRank from "./components/user-rank";
 import Sort from "./components/sort";
 import Stats from "./components/stats";
 import Films from "./components/films/films";
+import Statistics from "./components/statistics";
 import MoviesModel from "./models/movies";
 import CommentsModel from "./models/comments";
 import FilterController from "./controllers/Filter-controller";
 import PageController from "./controllers/page-controller";
+
 import {generateFilms, generateComments} from "./mocks/films";
 import {render} from "./util/dom-util";
 
@@ -42,3 +44,15 @@ const statisticsComponent = new Statistics(moviesModel);
 render(siteMainElement, statisticsComponent);
 statisticsComponent.show(moviesModel.films);
 statisticsComponent.hide();
+
+filterController.setOnMenuItemClick((menuItem) => {
+  if (menuItem === `stats`) {
+    pageController.hide();
+    sortComponent.hide();
+    statisticsComponent.show(moviesModel.films);
+  } else {
+    pageController.show();
+    sortComponent.show();
+    statisticsComponent.hide();
+  }
+});
