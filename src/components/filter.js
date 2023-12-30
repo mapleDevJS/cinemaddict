92e89a52d852b787f38de0ab055b934f2f4f7d55
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2023-12-30 14:43:44 -0500

    The commit message does not match with the code differences. Here's the correct one.
    Refactor filter.js to simplify markup creation
    
    Simplified the markup creation process in filter.js by refactoring two separate methods into one. This change increases code efficiency by notably reducing the amount of code and eliminating redundancy. This will also improve overall readability and maintainability.

--- src/components/filter.js
+++ src/components/filter.js
@@ -4,71 +4,47 @@ import {FilterType} from "../util/filter";
 const LINK_TAG_NAME = `A`;
 
 export default class Filter extends AbstractComponent {
-  constructor(filters, menuItems) {
-    super();
+    constructor(filters, menuItems) {
+        super();
+        this._filters = filters;
+        this._menuItems = menuItems;
+    }
 
-    this._filters = filters;
-    this._menuItems = menuItems;
-  }
-
-  getTemplate() {
-    const filtersMarkup = this._filters.map((filter) => this._createFilterMarkup(filter)).join(`\n`);
-    const menuMarkup = this._menuItems.map((filter) => this._createMenuMarkup(filter)).join(`\n`);
-
-    return (
-      `<nav class="main-navigation">
-        <div class="main-navigation__items">
-          ${filtersMarkup}
-        </div>
+    getTemplate() {
+        const filtersMarkup = this._filters.map(this._createMarkup).join(`\n`);
+        const menuMarkup = this._menuItems.map(this._createMarkup).join(`\n`);
+        return (
+            `<nav class="main-navigation">
+        <div class="main-navigation__items">${filtersMarkup}</div>
         ${menuMarkup}
       </nav>`
-    );
-  }
-
-  setFilterChangeListener(listener) {
-    this.getElement().addEventListener(`click`, (evt) => {
-      evt.preventDefault();
-
-      if (evt.target.tagName !== LINK_TAG_NAME) {
-        return;
-      }
-
-      const filterName = this._getFilterNameByHash(evt.target.hash);
-
-      listener(filterName);
-    });
-  }
-
-  _createFilterMarkup({type, name, count, active}) {
-    return (
-      `<a href="#${type}"
-        class="main-navigation__item ${active ? `main-navigation__item--active` : ``}">
-          ${name}
-        ${this._showCount(type, count)}
-      </a>`
-    );
-  }
-
-  _createMenuMarkup({type, name, active}) {
-    return (
-      `<a href="#${type}" class="main-navigation__additional ${active ? `main-navigation__additional--active` : ``}">
-        ${name}
-      </a>`
-    );
-  }
-
-  _showCount(type, count) {
-    if (type !== FilterType.ALL) {
-      return (
-        `<span class="main-navigation__item-count">
-          ${count}
-        </span>`
-      );
+        );
     }
-    return ``;
-  }
 
-  _getFilterNameByHash(hash) {
-    return hash.substring(1, hash.length);
-  }
+    setFilterChangeListener(listener) {
+        this.getElement().addEventListener(`click`, (evt) => {
+            evt.preventDefault();
+            if (evt.target.tagName !== LINK_TAG_NAME) {
+                return;
+            }
+            const filterName = this._getFilterNameByHash(evt.target.hash);
+            listener(filterName);
+        });
+    }
+
+    _createMarkup({type, name, count, active}) {
+        const itemClass = (count !== undefined) ? "main-navigation__item" : "main-navigation__additional";
+        const countBlock = (count !== undefined && type !== FilterType.ALL) ? `<span class="main-navigation__item-count">${count}</span>` : ``;
+        const isActive = active ? `${itemClass}--active` : ``;
+        return (
+            `<a href="#${type}" class="${itemClass} ${isActive}">
+        ${name}
+        ${countBlock}
+      </a>`
+        );
+    }
+
+    _getFilterNameByHash(hash) {
+        return hash.substring(1, hash.length);
+    }
 }
